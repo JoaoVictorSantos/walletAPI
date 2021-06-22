e2027e000c29967705d440d3203fb9eef54ec12d
JoaoVictorSantos <victordossantos.joao06@gmail.com>
2021-06-22 10:14:32 -0300

    Projeto inicial

---
+++ src/test/java/com/wallet/WalletApplicationTests.java
@@ -0,0 +1,19 @@
+package com.wallet;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")//profile que vai ser ativado.
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
