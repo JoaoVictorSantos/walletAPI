3310b4ca3a9f5fd07ea325ef71df9a960e84a12b
JoaoVictorSantos <victordossantos.joao06@gmail.com>
2021-06-22 14:09:44 -0300

    Finalizado o repositório do usuário

--- src/test/java/com/wallet/WalletApplicationTests.java
+++ src/test/java/com/wallet/WalletApplicationTests.java
@@ -1,6 +1,8 @@
 package com.wallet;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
