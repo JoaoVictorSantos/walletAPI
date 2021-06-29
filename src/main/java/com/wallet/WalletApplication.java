e852a343b645779617c31fa1d5671585d9fc9899
JoaoVictorSantos <victordossantos.joao06@gmail.com>
2021-06-29 14:36:27 -0300

    Travis test

--- src/main/java/com/wallet/WalletApplication.java
+++ src/main/java/com/wallet/WalletApplication.java
@@ -3,8 +3,10 @@ package com.wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
+@ActiveProfiles("prod")
 @EnableCaching
 public class WalletApplication {
 
