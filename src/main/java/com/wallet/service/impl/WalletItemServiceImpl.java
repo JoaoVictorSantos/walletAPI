9933460c3fd326d39fbc83e625ed010f64ed2815
JoaoVictorSantos <victordossantos.joao06@gmail.com>
2021-06-29 11:00:50 -0300

    Implementação do cache

--- src/main/java/com/wallet/service/impl/WalletItemServiceImpl.java
+++ src/main/java/com/wallet/service/impl/WalletItemServiceImpl.java
@@ -6,6 +6,8 @@ import com.wallet.repository.WalletItemRepository;
 import com.wallet.service.WalletItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -25,6 +27,7 @@ public class WalletItemServiceImpl implements WalletItemService {
     WalletItemRepository repository;
 
     @Override
+    @CacheEvict(value = "findByWalletAndType", allEntries = true)
     public WalletItem save(WalletItem wi) {
         return repository.save(wi);
     }
@@ -36,6 +39,7 @@ public class WalletItemServiceImpl implements WalletItemService {
     }
 
     @Override
+    @Cacheable(value = "findByWalletAndType")
     public List<WalletItem> findByWalletAndType(Long wallet, TypeEnum type) {
         return repository.findByWalletIdAndType(wallet, type);
     }
@@ -51,6 +55,7 @@ public class WalletItemServiceImpl implements WalletItemService {
     }
 
     @Override
+    @CacheEvict(value = "findByWalletAndType", allEntries = true)
     public void deleteById(Long id) {
         repository.deleteById(id);
     }
