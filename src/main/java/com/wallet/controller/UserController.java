de9bee80c52573ea1ec274fb1807842475edfbc4
JoaoVictorSantos <victordossantos.joao06@gmail.com>
2021-07-19 10:32:41 -0300

    Buscando o usuário logado

--- src/main/java/com/wallet/controller/UserController.java
+++ src/main/java/com/wallet/controller/UserController.java
@@ -5,16 +5,14 @@ import com.wallet.entity.User;
 import com.wallet.response.Response;
 import com.wallet.service.UserService;
 import com.wallet.util.Bcrypt;
+import com.wallet.util.Util;
 import com.wallet.util.enums.RoleEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
@@ -33,6 +31,20 @@ public class UserController {
         User user = service.save(this.convertDtoToEntity(dto));
         response.setData(this.convertEntityToDto(user));
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/logged-in")
+    public ResponseEntity<Response<UserDTO>> LoggedIn() {
+        Response<UserDTO> response = new Response<UserDTO>();
+        Long id = Util.getAuthenticatedUserId();
+        if(id == null){
+            response.getErrors().add("Você precisa estar logado para acessar essa funcionalidade!");
+            return ResponseEntity.badRequest().body(response);
+        }
+        UserDTO dto = new UserDTO();
+        dto.setId(Util.getAuthenticatedUserId());
+        response.setData(dto);
+        return ResponseEntity.ok().body(response);
     }
 
     private User convertDtoToEntity(UserDTO dto){
